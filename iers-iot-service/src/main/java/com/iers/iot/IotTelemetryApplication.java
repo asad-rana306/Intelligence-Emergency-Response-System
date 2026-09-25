d251907a1cf95e0932df92ec6b03ebcd2ae4cc9b
Rana Asad <asadcodecraft@gmail.com>
2026-09-26 02:05:15 +0500

    main method is added

---
+++ iers-iot-service/src/main/java/com/iers/iot/IotTelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.iers.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class IotTelemetryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IotTelemetryApplication.class, args);
+    }
+}
